87e62a5af9cb3ff182ec751f57e8418a0fc35e4f
Antoine <antoine.bouvard01@hotmail.fr>
2021-02-08 11:15:50 +0100

    rendu

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller;
+
+require(__DIR__ . "/../vendor/autoload.php");
+
+
+use App\Kernel;
+$kernel = new Kernel();
+$kernel->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
